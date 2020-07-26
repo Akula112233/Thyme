9c1c42a20c049bd74ec487f9da6b2862749beef2
Charan <phazia.co@gmail.com>
2020-07-26 01:46:57 -0500

    finished the UI for the text chat

--- src/chat/ChatItem.js
+++ src/chat/ChatItem.js
@@ -4,7 +4,7 @@ import './chat.css'
 function ChatItem(props) {
     if (props.type == 1) {
         return (
-            <div style={{marginTop: props.marginTop}}className= "chat-item">
+            <div style={{marginTop: props.marginTop, justifyContent: "flex-start"}}className= "chat-item">
                 <div className= "chat-item-content-container">
                     <p style={{display: props.display}} className= "chat-item-author">{props.author.toUpperCase()}</p>
                     <div className= "chat-background">
@@ -15,11 +15,11 @@ function ChatItem(props) {
         )
     }
     return (
-        <div style={{marginTop: props.marginTop}}className= "chat-item">
-            <div className= "chat-item-content-container">
-                <p style={{display: props.display}} className= "chat-item-author">{props.author.toUpperCase()}</p>
-                <div style={{backgroundColor: "rgba(28, 55, 91, 0.8)"}}className= "chat-background">
-                    <p style={{color: "white"}} className= "chat-item-content">{props.content}</p>
+        <div style={{marginTop: props.marginTop, justifyContent: "flex-end"}} className= "chat-item">
+            <div style={{justifyContent: "flex-end"}} className= "chat-item-content-container">
+                <p style={{display: props.display, textAlign: "right"}} className= "chat-item-author">{props.author.toUpperCase()}</p>
+                <div style={{backgroundColor: "rgba(28, 55, 91, 0.8)", borderRadius: "15px 0px 15px 15px"}}className= "chat-background">
+                    <p style={{color: "white", textAlign: "right"}} className= "chat-item-content">{props.content}</p>
                 </div>
             </div>
         </div>
