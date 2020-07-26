93dd3c4365d587e81f704c95133f25ad311272c2
Charan <phazia.co@gmail.com>
2020-07-25 22:07:58 -0500

    clean up on UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Chat from './chat/chat'
+import SideBar from './sidebar/sidebar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Chat></Chat>
+      <SideBar></SideBar>
     </div>
   );
 }
