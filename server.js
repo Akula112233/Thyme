a091f9c95ee23e713c59c8db0c08c25715a02b04
d0-rb <henryandrecastillo@gmail.com>
2020-07-26 03:44:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const http = require("http")
+const express = require("express")
+const https = require("https")
 const socketIo = require("socket.io")
-const index = require("./routes/index");
+const index = require("./routes/index")
+const fs = require("fs")
+const helmet = require("helmet")
 
+const options = {
+	key: fs.readFileSync("/home/ec2-user/Thyme/key.pem"),
+	cert: fs.readFileSync("/home/ec2-user/Thyme/cert.pem")
+}
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
 
 const app = express()
+app.use(helmet())
 app.use(index)
 
-const server = http.createServer(app)
+const server = http.createServer(options, app)
 
 const io = socketIo(server)
 
