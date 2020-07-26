74929b432f241872790e8e72355e390384474f16
Charan <phazia.co@gmail.com>
2020-07-25 21:15:22 -0500

    start chat stuff

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './chat/chat'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat></Chat>
+    </div>
+  );
+}
+
+export default App;
